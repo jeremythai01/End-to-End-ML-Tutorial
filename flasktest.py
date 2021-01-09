73e7946e1bda9219c526dfcfc9669967fb7cec38
Jeremy Thai <jeremythai01@gmail.com>
2021-01-08 20:49:04 -0500

    changed VADER to SVM

--- flasktest.py
+++
@@ -1,2 +0,0 @@
-from flask import Flask
-from flaskext.mysql import MySQL
---
+++ src/ml/model.py
@@ -0,0 +1,42 @@
+import os
+import pickle
+import wget
+from ml.preprocessing import Preprocessing
+from functools import reduce
+
+class SentimentAnalysisSVM():
+
+    def __init__(self):
+
+        model_name = "sentiment_svm_model.pkl"
+        model_path = f'./ml/models/{model_name}'
+
+        if model_name not in os.listdir('./ml/models/'):
+            print(f'downloading the trained model {model_name}')
+            wget.download(
+                "https://github.com/jeremythai01/End-to-End-ML/releases/download/model/sentiment_svm_model.pkl",
+                out=model_path
+            )
+        else:
+            print('model already saved to api/ml/models')
+
+        self.__model = pickle.load(open('./ml/models/sentiment_svm_model.pkl','rb'))
+        self.__preprocessing = Preprocessing()
+
+
+    def predict_sentiment(self, df):
+
+        df = self.__preprocessing.preprocess_text(df)
+        data_vector = self.__preprocessing.vectorize(df)
+        prediction_proba = self.__model.predict_proba(data_vector) #0 = negative, #1 = positive
+        prediction_score = [reduce(lambda x, y: round(y - x, 4), row) for row in prediction_proba]
+    
+        df['sentiment'] = prediction_score
+        
+
+        df.drop(columns=['cleaned body'], inplace=True)
+        
+        return df
+
+        
+            
