c1673e7f4a394e2ccfdddf59c711b269079e673e
Jeremy Thai <jeremythai01@gmail.com>
2021-01-08 20:55:30 -0500

    changed stream to etl

---
+++ src/etl/stream_handler.py
@@ -0,0 +1,42 @@
+class StreamHandler():
+
+    def __init__(self, db_connection):
+        self.__db_connection = db_connection
+
+    def stream_to_database(self, df):
+
+        i_c = 0
+        for i in range(len(df.axes[0])):
+            
+            try:
+
+                to_add = [df['subreddit'][i], df['author'][i], df['body'][i], df['date'][i], float(df['sentiment'][i])]
+                insert_query = """
+                                INSERT IGNORE INTO Comment 
+                                VALUES (DEFAULT, %s, %s, %s, %s, %s)
+                                """
+                self.__db_connection.query(insert_query, to_add)
+                self.__db_connection.commit()
+                i_c += 1
+
+            except AttributeError:
+                continue
+
+        print(f'Streamed {i_c} comments')
+
+    def close_db_connection(self):
+        self.__db_connection.close()
+
+
+
+
+    
+
+
+
+
+
+        
+
+
+
