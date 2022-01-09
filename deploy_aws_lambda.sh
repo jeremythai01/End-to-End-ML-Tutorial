aec8e435a981e2641a2a192aa2076aba84b2af06
Jeremy Thai <github@jetha.anonaddy.com>
2022-01-09 14:22:24 -0500

    added deploy to lambda script v2

--- deploy_aws_lambda.sh
+++ deploy_aws_lambda.sh
@@ -1,5 +1,9 @@
 source .env
 
+docker-compose build
+
+docker-compose push
+
 aws lambda update-function-code --function-name ScrapePushReddit --image-uri $AWS_ECR/data_scrape_pipeline:latest
 
 aws lambda update-function-code --function-name TransformLoadDB --image-uri $AWS_ECR/data_prep_pipeline:latest
